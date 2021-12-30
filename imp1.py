bf91cbb439c35fb1d874526f28d791a67ce239d9
jungmo Lee <dlsxprmfkf1234@naver.com>
2021-12-30 14:09:41 +0900

    first commit

---
+++ imp1.py
@@ -0,0 +1,23 @@
+print(3*5+2)
+print("ㅋ"*9)
+print(not(10>5))
+print(True)
+print(not False)
+
+#문자열 합쳐 출력
+ani = "강아지"
+age = 4
+print("우리집 " + ani + "는 "+ str(age) + "살이에요")
+
+is_adult = age>=3
+print(is_adult)
+
+'''주석 처리'''
+# ctrl+/ 
+
+print(abs(-4))
+print(pow(2,4))
+print(round(4.29,1))
+print(min(-1,4))
+
+# floor, ceil, sqrt
